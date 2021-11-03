7b12bef29beb19338704c92c63afed70cf5ac0a5
Giulialutke <giulialutke@gmail.com>
2021-11-02 22:48:23 -0300

    Static charts

---
+++ frontend/src/components/BarChart/index.tsx
@@ -0,0 +1,39 @@
+import Chart from 'react-apexcharts';
+
+const BarChart = () => {
+
+    const options = {
+        plotOptions: {
+            bar: {
+                horizontal: true,
+            }
+        },
+    };
+
+
+    const mockData = {
+        labels: {
+            categories: ['Anakin', 'Lohana Queiroga', 'Kal-El', 'Logan', 'Padmé']
+        },
+        series: [
+            {
+                name: "% Sucesso",
+                data: [43.6, 7.0, 67.7, 45.6, 71.1]
+            }
+        ]
+    };
+
+    return (
+        <Chart
+            options={{ ...options, xaxis: mockData.labels }}
+            series={mockData.series}
+            type="bar"
+            height="240"
+        />
+
+
+    );
+
+}
+
+export default BarChart;
